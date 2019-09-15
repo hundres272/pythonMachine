6e9d88158939c40dbacdf394de8ee3808cc5fb2e
Hugo <hugoandres272@gmail.com>
2019-09-14 22:38:35 -0500

    Comenzando curso python platzi

---
+++ 2. Curso de python/1.ProgramaUno.py
@@ -0,0 +1,14 @@
+import turtle
+
+window = turtle.Screen()    #generar una ventan
+david = turtle.Turtle()
+david.forward(50)           #que se mueva una distancia de 50 hacia la derecha
+david.left(90)              #gire 90 grados
+david.forward(50)
+david.left(90)
+david.forward(50)
+david.left(90)
+david.forward(50)
+david.left(90)
+
+turtle.mainloop()           #para que no cierre la ventana
---
+++ 2. Curso de python/2. PreguntaNombre.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+name = str(input('Cuál es tu nombre: '))
+print('Hola ' + name + '!')

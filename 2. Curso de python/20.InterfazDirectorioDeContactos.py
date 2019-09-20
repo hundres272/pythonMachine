d839b9a06ae7600559492c326c8ca86c154cd297
Hugo <hugoandres272@gmail.com>
2019-09-20 11:14:20 -0500

    Terminando proyecto:aplicacion de terminal, fase 6

--- 2. Curso de python/20.InterfazDirectorioDeContactos.py
+++ 2. Curso de python/20.InterfazDirectorioDeContactos.py
@@ -1,10 +1,63 @@
 # -*- coding: utf-8 -*-
+import csv
 
 class ContactBook:
     def __init__(self):
         self._contacts = []
     def add(self,name,phone,email):
-        print('name: {},phone: {},email: {}'.format(name,phone,email))
+        contact = Contact(name,phone,email)
+        self._contacts.append(contact)
+        self._save()
+    def show_all(self):
+        for contact in self._contacts:
+            self._print_contact(contact)
+    def delete(self,name):
+        for idx,contact in enumerate(self._contacts):
+            if contact._name.lower() == name.lower():
+                del self._contacts[idx]
+                self._save()
+                break
+    def search(self,name):
+        for contact in self._contacts:
+            if contact._name.lower() == name.lower():
+                self._print_contact(contact)
+    def update(self,name):
+        for contact in self._contacts:
+            if contact._name.lower() == name.lower():
+                option = str(input('''
+                Que dato desea actualizar:
+                [n]ombre
+                [e]mail
+                [t]eléfono
+                '''))
+                if option == 'n':
+                    contact._name = str(input('Nombre: '))
+                elif option == 'e':
+                    contact._email = str(input('Email: '))
+                elif option == 't':
+                    contact._phone = str(input('Teléfono: '))
+                else:
+                    print('***       Comando no encontrado       ***')
+                return
+        self._not_found()
+
+    def _save(self):
+        with open('contacts.csv','w') as f:
+            writer = csv.writer(f)
+            writer.writerow( ('name', 'phone', 'email'))
+            for contact in self._contacts:
+                writer.writerow((contact._name, contact._phone, contact._email))
+
+    def _print_contact(self,contact):
+        print('--- * --- * --- * --- * --- * --- * --- * ---')
+        print('Nombre: {}'.format(contact._name))
+        print('Telefono: {}'.format(contact._phone))
+        print('Email: {}'.format(contact._email))
+        print('--- * --- * --- * --- * --- * --- * --- * ---')
+    def _not_found(self):
+        print('***********')
+        print('No encontado!!!')
+        print('***********')
 
 class Contact:
     def __init__(self,name,phone,email):
@@ -15,6 +68,17 @@ class Contact:
 def run():
 
     contact_book = ContactBook()
+
+    try:
+        with open('contacts.csv','r') as f:
+            reader = csv.reader(f)
+            for idx,row in enumerate(reader):
+                if row:
+                    if idx == 0:
+                        continue
+                    contact_book.add(row[0],row[1],row[2])
+    except FileNotFoundError:
+        print('No se encontró el archivo contacts.cvs')
 
     while True:
         command = str(input('''
@@ -35,16 +99,19 @@ def run():
             contact_book.add(name,phone,email)
 
         elif command == 'ac':
-            print('actualizar contacto')
+            name = str(input('Escriba el nombre del contacto: '))
+            contact_book.update(name)
 
         elif command == 'b':
-            print('buscar contacto')
+            name = str(input('Escriba el nombre del contacto: '))
+            contact_book.search(name)
 
         elif command == 'e':
-            print('eliminar contacto')
+            name = str(input('Escriba el nombre del contacto: '))
+            contact_book.delete(name)
 
         elif command == 'l':
-            print('listar contactos')
+            contact_book.show_all()
 
         elif command == 's':
             break
